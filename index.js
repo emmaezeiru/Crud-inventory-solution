5b2b0415d340c1c2b1dbbca4a95064a470e4f744
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-06-11 21:19:49 +0100

    connected mongodb database

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const  express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/view/index.html")
+    res.sendFile(__dirname + "/views/index.html")
 });
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.listen(port, () =>{
     console.log(`i am listening on ${4000}`);
