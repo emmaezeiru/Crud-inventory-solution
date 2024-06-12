0e998a000282c2770d85e8c0d1cbffb7d363c0a9
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-06-12 18:22:06 +0100

    changed item to items to debug error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const {Schema} = mongoose;
 
 const ItemSchema = new Schema({
-    item: {
+    items: {
         type: String
     }
 });
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 
 app.post("/api/item", async(req, res)=>{
     const itemObj = new Item({
-        item: req.body.item
+        items: req.body.item
     })
     const item = await itemObj.save()
     res.json(item)
