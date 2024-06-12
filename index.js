d144517f9aa6e1512b080393c4af05bf943a48bc
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-06-12 17:50:57 +0100

    post request for items

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 require('dotenv').config();
 const  express = require('express');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 const port = 4000;
+const cors = require('cors');
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const {Schema} = mongoose;
+
+const ItemSchema = new Schema({
+    item: String,
+});
+const Item = mongoose.model("Item", ItemSchema);
+
+const InventorySchema = new Schema({
+    item_id: {
+        type: String, required: true
+    },
+    price: Number,
+    quatity: Number,
+})
+const Inventory = mongoose.model("Inventory", InventorySchema);
+
+app.use(cors())
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended: true}))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 });
 
-app.use("/public", express.static(__dirname + "/public"));
+app.post("/api/item", async(req, res)=>{
+    console.log(req.body)
+    const itemObj = new Item;
+})
+
+app.post("api/item/:inventory", async(req, res) => {
+    res.json()
+})
+
+app.get("", req, res => {
+    res.json()
+})
+
+app.put("", req, res => {
+    res.json()
+})
+
+app.delete("", (req, res) =>{
+    res.json()
+})
+
+
+
+
+
+
+
 
 app.listen(port, () =>{
     console.log(`i am listening on ${4000}`);
